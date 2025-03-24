cf6081df13ba8496e1c35010a9f3a215e4556c78
Reatlilwe Maroga <rea.maroga@gmail.com>
2025-03-24 19:30:55 +0200

    next appointment and welcome componenets for dashboard

--- health-link/src/pages/Dashboard.jsx
+++ health-link/src/pages/Dashboard.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import '/src/assets/style.css';
+import "/src/assets/style.css";
 import Navigation from "../components/Navigation";
+import NextAppointment from "../components/NextAppointment";
+import Welcome from "../components/Welcome";
 
 const Dashboard = () => {
-    
-    return(
-<div className="app-container">
-<Navigation/>
-</div>
-    )
-}
+  return (
+    <div className="app-container">
+      <Navigation />
+
+      <div>
+        <Welcome />
+      </div>
+
+      <div>
+        <NextAppointment />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
